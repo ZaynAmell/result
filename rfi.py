cdb3a50373a0fa42e7b275a1ce36582d2f824529
desktop <zayn872240488@gmail.com>
2021-06-21 22:16:02 -0400

    Recent work

--- rfi.py
+++ rfi.py
@@ -1,5 +1,7 @@
 import blimpy as bl
 from scipy import signal
+import numpy as np
+from scipy import interpolate
 
 def localmax(wf,pro=(20000,3.0e6),dist=30):
     """This funcation takes into a waterfall file
@@ -24,7 +26,7 @@ def localmax(wf,pro=(20000,3.0e6),dist=30):
                 if j==length-1:
                     end=plot_f[peak[0][i+1:][j]]
                     i+=j+1
-                    rfi.append((start,end))
+                    rfi.append([start,end])
                     break
                 else:
                     if (abs(plot_f[peak[0][i+1:][j]]-end)<=dist):
@@ -32,11 +34,22 @@ def localmax(wf,pro=(20000,3.0e6),dist=30):
                     else:
                         i+=j+1
                         if(j==0):
-                            rfi.append((start,start))
+                            rfi.append([start,start])
                         else:
-                            rfi.append((start,end))
+                            rfi.append([start,end])
                         break
         else:
-            rfi.append((start,start))
+            rfi.append([start,start])
             i+=1
     return rfi
+
+def baseline(wf,spl_order=None):
+    channel_width=wf.container.n_channels_in_file
+    if spl_order==None:
+        spl_order=8
+    x=wf.get_freqs()
+    integrated_channel=wf.get_power()[1]
+    knots = np.arange(0, channel_width, channel_width//spl_order+1)
+    spl = interpolate.splrep(x, integrated_channel,t=wf.container.f_start+knots[1:]*wf.header['foff'])
+    chan_fit = interpolate.splev(x, spl)
+    return chan_fit
